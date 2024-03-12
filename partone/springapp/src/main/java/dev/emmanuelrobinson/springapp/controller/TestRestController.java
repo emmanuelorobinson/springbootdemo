4f104d813e9460b1217b1ec0ef3be3d1ed6d2726
emmanuelorobinson <manuelejeay@gmail.com>
2024-03-12 14:02:51 +0400

    Add REST controller with hello endpoint and update server port

--- partone/springapp/src/main/java/dev/emmanuelrobinson/springapp/controller/TestRestController.java
+++ partone/springapp/src/main/java/dev/emmanuelrobinson/springapp/controller/TestRestController.java
@@ -1,2 +1,13 @@
-package dev.emmanuelrobinson.springapp.controller;public class TestRestController {
+package dev.emmanuelrobinson.springapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
 }
