3d6b080ee12ad50bb2fda2c8d845c5f6a173f4c3
Emmanuel Robinson <manuelejeay@gmail.com>
2024-05-02 21:07:38 +0400

    chore: Add application.properties, maven-wrapper.properties, .gitignore, RestdemoApplicationTests, RestdemoApplication, DemoRestController, StudentNotFoundException, Student, and StudentErrorResponse classes

---
+++ partfour/restdemo/src/main/java/dev/emmanuelrobinson/restdemo/RestdemoApplication.java
@@ -0,0 +1,14 @@
+package dev.emmanuelrobinson.restdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdemoApplication.class, args);
+	}
+
+//	PART 22
+}
